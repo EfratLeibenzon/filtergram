f7797710058e577d984ac070c299523157f67377
Efrat Leibenzon <efrat.leibenzon@gmail.com>
2023-06-02 17:16:49 +0300

    cloudinary added

--- src/cmps/story-add.jsx
+++ src/cmps/story-add.jsx
@@ -1,29 +1,38 @@
 import { useState } from "react"
 import { storyService } from "../services/story.service.local"
+import { fill } from "@cloudinary/url-gen/actions/resize";
+import { CloudinaryImage } from '@cloudinary/url-gen';
 
 export function StoryAdd() {
-    const [storyToEdit, setStoryToEdit] = useState(storyService.getEmptyStory())
+    // const [storyToEdit, setStoryToEdit] = useState(storyService.getEmptyStory())
+    const myImage = new CloudinaryImage('sample', { cloudName: 'duxmabf4n' }).resize(fill().width(100).height(150))
 
-    function handleChange({ target }) {
-        console.log(target.files)
-        setStoryToEdit({ ...storyToEdit, imgUrl: target.value })
-    }
+    // function handleChange({ target }) {
+    //     console.log(target.files)
+    //     setStoryToEdit({ ...storyToEdit, imgUrl: target.value })
+    // }
 
-    function toggleHidden() {
 
-    }
 
     return (
-        <div className="story-add">
-            {!storyToEdit.imgUrl && (
-                <section className="img-loader">
-                    <input onChange={handleChange} type="file" id="file-input" name="ImageStyle" />
-                </section>)}
-            {storyToEdit.imgUrl && (
-                <section className="img-to-edit">
-                    <img src={storyToEdit.imgUrl} alt="" />
-                </section>)}
+        <div>
+            <AdvancedImage cldImg={myImage} />
         </div>
-
     )
+
+    // return (
+    // <div className="story-add">
+    //     {!storyToEdit.imgUrl && (
+    //         <section className="img-loader">
+    //             <input onChange={handleChange} type="file" id="file-input" name="ImageStyle" />
+    //         </section>)}
+    //     {storyToEdit.imgUrl && (
+    //         <section className="img-to-edit">
+    //             <img src={storyToEdit.imgUrl} alt="" />
+    //         </section>)}
+    // </div>
+
+
+
+    // )
 }
