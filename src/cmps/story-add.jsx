130541029dfc87349d429c29b5b343186458d4d8
Efrat Leibenzon <efrat.leibenzon@gmail.com>
2023-06-03 12:05:34 +0300

    conflict solved

--- src/cmps/story-add.jsx
+++ src/cmps/story-add.jsx
@@ -3,16 +3,6 @@
 // import { fill } from "@cloudinary/url-gen/actions/resize";
 // import { CloudinaryImage } from '@cloudinary/url-gen';
 
-<<<<<<< HEAD
-export function StoryAdd() {
-    const [storyToEdit, setStoryToEdit] = useState(storyService.getEmptyStory())
-    const myImage = new CloudinaryImage('sample', { cloudName: 'duxmabf4n' }).resize(fill().width(100).height(150))
-
-    function handleChange({ target }) {
-        console.log(target.files)
-        setStoryToEdit({ ...storyToEdit, imgUrl: target.value })
-    }
-=======
 // export function StoryAdd() {
 //     // const [storyToEdit, setStoryToEdit] = useState(storyService.getEmptyStory())
 //     const myImage = new CloudinaryImage('sample', { cloudName: 'duxmabf4n' }).resize(fill().width(100).height(150))
@@ -21,7 +11,6 @@ export function StoryAdd() {
 //     //     console.log(target.files)
 //     //     setStoryToEdit({ ...storyToEdit, imgUrl: target.value })
 //     // }
->>>>>>> 4d8257b891ddd187e82c688851559e02e0189bbf
 
 
 
@@ -31,28 +20,6 @@ export function StoryAdd() {
 //         </div>
 //     )
 
-<<<<<<< HEAD
-    function AdvancedImage(cldImg) {
-        return <img src={cldImg} alt="" />
-    }
-
-    return (
-        <div className="story-add">
-            {!storyToEdit.imgUrl && (
-                <section className="img-loader">
-                    <input onChange={handleChange} type="file" id="file-input" name="ImageStyle" />
-                </section>)}
-            {storyToEdit.imgUrl && (
-                <section className="img-to-edit">
-                    <img src={storyToEdit.imgUrl} alt="" />
-                </section>)}
-        </div>
-
-
-
-    )
-}
-=======
 
 //     function AdvancedImage(cldImg) {
 //         // <img src={cldImg} alt="" />
@@ -74,4 +41,3 @@ export function StoryAdd() {
 
 //     // )
 // }
->>>>>>> 4d8257b891ddd187e82c688851559e02e0189bbf
