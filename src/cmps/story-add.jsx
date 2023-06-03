a4fa184ad918bc92bcdaef9f4d15dd2d4a5ed801
Efrat Leibenzon <efrat.leibenzon@gmail.com>
2023-06-03 11:57:34 +0300

    upload widget added

--- src/cmps/story-add.jsx
+++ src/cmps/story-add.jsx
@@ -4,13 +4,13 @@ import { fill } from "@cloudinary/url-gen/actions/resize";
 import { CloudinaryImage } from '@cloudinary/url-gen';
 
 export function StoryAdd() {
-    // const [storyToEdit, setStoryToEdit] = useState(storyService.getEmptyStory())
+    const [storyToEdit, setStoryToEdit] = useState(storyService.getEmptyStory())
     const myImage = new CloudinaryImage('sample', { cloudName: 'duxmabf4n' }).resize(fill().width(100).height(150))
 
-    // function handleChange({ target }) {
-    //     console.log(target.files)
-    //     setStoryToEdit({ ...storyToEdit, imgUrl: target.value })
-    // }
+    function handleChange({ target }) {
+        console.log(target.files)
+        setStoryToEdit({ ...storyToEdit, imgUrl: target.value })
+    }
 
 
 
@@ -20,19 +20,23 @@ export function StoryAdd() {
         </div>
     )
 
-    // return (
-    // <div className="story-add">
-    //     {!storyToEdit.imgUrl && (
-    //         <section className="img-loader">
-    //             <input onChange={handleChange} type="file" id="file-input" name="ImageStyle" />
-    //         </section>)}
-    //     {storyToEdit.imgUrl && (
-    //         <section className="img-to-edit">
-    //             <img src={storyToEdit.imgUrl} alt="" />
-    //         </section>)}
-    // </div>
+    function AdvancedImage(cldImg) {
+        return <img src={cldImg} alt="" />
+    }
+
+    return (
+        <div className="story-add">
+            {!storyToEdit.imgUrl && (
+                <section className="img-loader">
+                    <input onChange={handleChange} type="file" id="file-input" name="ImageStyle" />
+                </section>)}
+            {storyToEdit.imgUrl && (
+                <section className="img-to-edit">
+                    <img src={storyToEdit.imgUrl} alt="" />
+                </section>)}
+        </div>
 
 
 
-    // )
+    )
 }
---
+++ src/cmps/upload-widget.jsx
@@ -0,0 +1,6 @@
+
+
+export function UploadWidget() {
+
+    return
+}
