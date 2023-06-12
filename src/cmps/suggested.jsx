9f4f483c839a5f1152bae796323671d78d976084
Efrat Leibenzon <efrat.leibenzon@gmail.com>
2023-06-12 11:01:52 +0300

    sync

--- src/cmps/suggested.jsx
+++ src/cmps/suggested.jsx
@@ -10,15 +10,9 @@ export function Suggested() {
         <div className="suggestions">
             <div className="suggestion-header">
                 <div className="suggestion-header-info">
-<<<<<<< HEAD
-                    <NavLink to={`/profile/${user._id}`}><img src={user.userImg.url} style={user.userImg.style} /></NavLink>
-                    <div className="suggestion-user-name">
-                        <NavLink to={`/profile/${user._id}`}>     <a>{user.userName}</a></NavLink>
-=======
                     <NavLink to={`/profile/${user._id}`}><img src={user.userImg?.url} style={user.userImg?.style} /></NavLink>
                     <div className="suggestion-user-name">
                         <NavLink to={`/profile/${user._id}`}>{user.userName}</NavLink>
->>>>>>> 571bb363f5abe76dd5e4a28f8796c10ebc70c970
                         <span>{user.fullName}</span>
                         {/* <NavLink className="navbar-item-profile" to={`/profile/${user._id}`}><img src={user.userImg.url} style={user.userImg.style} />Profile</NavLink> */}
                     </div>
